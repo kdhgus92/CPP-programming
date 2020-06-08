442e1c01af85310b54df3272a9b65c69fed1434b
potash <35871896+potahyox@users.noreply.github.com>
2020-06-08 22:48:26 +0900

    Create Point.h

---
+++ ch04/Point.h
@@ -0,0 +1,21 @@
+#ifndef __HEADER_H__
+#define __HEADER_H__
+
+//#ifndef __POINT_H_
+//#define __POINT_H_
+
+class Point
+{
+private:
+	int x;
+	int y;
+
+public:
+	bool InitMembers(int xpos, int ypos);
+	int GetX() const;
+	int GetY() const;
+	bool SetX(int xpos);
+	bool SetY(int ypos);
+};
+
+#endif
