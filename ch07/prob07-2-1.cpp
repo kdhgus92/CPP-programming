538a00fdadcb3e02f66468b87064cfaf46537b92
potash <35871896+potahyox@users.noreply.github.com>
2020-06-18 22:58:33 +0900

    Create prob07-2-1.cpp

---
+++ ch07/prob07-2-1.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+class Rectangle
+{
+private:
+	int width;
+	int height;
+public:
+	Rectangle(int w, int h)
+		:width(w), height(h)
+	{ }
+	void ShowAreaInfo()
+	{
+		cout << "면적: " << width * height << endl;
+	}
+};
+
+class Square : public Rectangle
+{
+public:
+	Square(int n)
+		: Rectangle(n, n)
+	{}
+};
+
+int main(void)
+{
+	Rectangle rec(4, 3);
+	rec.ShowAreaInfo();
+
+	Square sqr(7);
+	sqr.ShowAreaInfo();
+	return 0;
+}
