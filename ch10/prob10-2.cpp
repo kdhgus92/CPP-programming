b27e128a90e21c394da850bee00b3074b6d90355
potash <35871896+potahyox@users.noreply.github.com>
2020-06-23 20:15:05 +0900

    Create prob10-2.cpp

---
+++ ch10/prob10-2.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+using namespace std;
+
+class Point
+{
+private:
+	int xpos, ypos;
+public:
+	Point(int x = 0, int y = 0) : xpos(x), ypos(y)
+	{}
+	void ShowPosition() const
+	{
+		cout << '[' << xpos << ", " << ypos << ']' << endl;
+	}
+	Point& operator++()
+	{
+		xpos += 1;
+		ypos += 1;
+		return *this;
+	}
+	friend Point& operator--(Point& ref);
+
+	Point operator-()
+	{
+		Point pos(-(this->xpos), -(this->ypos));
+		return pos;
+	}
+
+	friend Point operator~(Point& ref);
+};
+
+Point& operator--(Point& ref)
+{
+	ref.xpos -= 1;
+	ref.ypos -= 1;
+	return ref;
+}
+
+Point operator~(Point& ref)
+{
+	Point pos(ref.ypos, ref.xpos);
+	return pos;
+}
+
+int main(void)
+{
+	Point pos(1, 2);
+	++pos;
+	pos.ShowPosition();
+	--pos;
+	pos.ShowPosition();
+
+	++(++pos);
+	pos.ShowPosition();
+	--(--pos);
+	pos.ShowPosition();
+
+	cout << endl;
+
+	Point pos2 = -pos;
+	pos.ShowPosition();
+	pos2.ShowPosition();
+
+	cout << endl;
+	Point pos3 = ~pos;
+	pos.ShowPosition();
+	pos3.ShowPosition();
+
+	return 0;
+}
