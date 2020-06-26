39752e9647e2bd10d4c6d32ae3996a9e9a3901b8
potash <35871896+potahyox@users.noreply.github.com>
2020-06-26 19:09:35 +0900

    Update AccountHandler.h

--- ch11/OOP/AccountHandler.h
+++ ch11/OOP/AccountHandler.h
@@ -1,7 +1,7 @@
 /*
  * 파일이름 : AccountHandler.h
  * 작성자 : 권오현
- * 업데이트 정보: [2020, 06, 21] 파일버전 0.7
+ * 업데이트 정보: [2020, 06, 26] 파일버전 0.8
 */
 #ifndef __ACCOUNT_HANDLER_H__
 #define __ACCOUNT_HANDLER_H__
