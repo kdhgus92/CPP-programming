6ff8289c3deb3d28ffb562d76beb777520e1ae25
potash <35871896+potahyox@users.noreply.github.com>
2020-06-26 19:09:00 +0900

    Create AccountHandler.h

---
+++ ch11/OOP/AccountHandler.h
@@ -0,0 +1,32 @@
+/*
+ * 파일이름 : AccountHandler.h
+ * 작성자 : 권오현
+ * 업데이트 정보: [2020, 06, 21] 파일버전 0.7
+*/
+#ifndef __ACCOUNT_HANDLER_H__
+#define __ACCOUNT_HANDLER_H__
+#include "BankingCommonDecl.h"
+#include "Account.h"
+#include "AccountArray.h"
+
+class AccountHandler
+{
+private:
+	BoundCheckAccountPtrArray accArr; //변경
+	int accNum = 0;
+
+public:
+	AccountHandler();
+	void ShowMenu(void);
+	void MakeAccount(void);
+	void DepositMoney(void);
+	void WithdrawMoney(void);
+	void ShowAllAccInfo(void);
+	~AccountHandler();
+
+protected:
+	void MakeNormalAccount(void);
+	void MakeCreditAccount(void);
+};
+
+#endif
