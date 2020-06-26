da385485bf7cd7aa56c7b041c21253dd09cc986f
potash <35871896+potahyox@users.noreply.github.com>
2020-06-26 19:05:29 +0900

    Create AccountArray.h

---
+++ ch11/OOP/AccountArray.h
@@ -0,0 +1,30 @@
+/*
+ * 파일이름 : AccountArray.h
+ * 작성자 : 권오현
+ * 업데이트 정보: [2020, 06, 26] 파일버전 0.1
+*/
+#ifndef __ACCOUNT_ARRAY_H__
+#define __ACCOUNT_ARRAY_H__
+
+#include "Account.h"
+
+typedef Account* ACCOUNT_PTR;
+
+class BoundCheckAccountPtrArray
+{
+private:
+	ACCOUNT_PTR* arr;
+	int arrlen;
+	BoundCheckAccountPtrArray(const BoundCheckAccountPtrArray& arr) { }
+	BoundCheckAccountPtrArray& operator=(const BoundCheckAccountPtrArray& arr) { }
+public:
+	BoundCheckAccountPtrArray(int len) : arrlen(len)
+	{
+		arr = new ACCOUNT_PTR[len];
+	}
+	ACCOUNT_PTR& operator[] (int idx);
+	ACCOUNT_PTR operator[] (int idx) const;
+	int GetArrLen() const;
+	~BoundCheckAccountPtrArray();
+};
+#endif
