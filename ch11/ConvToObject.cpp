695c314ca0cb7fa31a961616a5c07fec0092ee93
potash <35871896+potahyox@users.noreply.github.com>
2020-06-26 18:36:23 +0900

    Create ConvToObject.cpp

---
+++ ch11/ConvToObject.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+class Number
+{
+private:
+	int num;
+public:
+	Number(int n = 0) : num(n)
+	{
+		cout << "Number(int n=0)" << endl;
+	}
+	Number& operator=(const Number& ref)
+	{
+		cout << "operator=()" << endl;
+		num = ref.num;
+		return *this;
+	}
+	void ShowNumber() { cout << num << endl; }
+};
+
+int main(void)
+{
+	Number num;
+	num = 30; //일치하지 않는 자료형 간 대입연산.
+	num.ShowNumber();
+	return 0;
+}
