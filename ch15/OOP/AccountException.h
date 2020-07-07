6c1967df5fe8cf02527e89eccad2cef3b3734b32
potash <35871896+potahyox@users.noreply.github.com>
2020-07-07 22:18:09 +0900

    Create AccountException.h

---
+++ ch15/OOP/AccountException.h
@@ -0,0 +1,36 @@
+/*
+ * 파일이름 : AccountException.h
+ * 작성자 : 권오현
+ * 업데이트 정보: [2020, 07, 07] 파일버전 0.12
+*/
+
+#ifndef __ACCOUNT_EXCEPTION_H__
+#define __ACCOUNT_EXCEPTION_H__
+
+class MinusException
+{
+private:
+	int exval;
+public:
+	MinusException(int val) : exval(val)
+	{}
+	void ShowExceptionInfo(void) const
+	{
+		cout << "입(출)금액 " << exval << "은 유효하지 않습니다!" << endl;
+	}
+};
+
+class InsuffException
+{
+private:
+	int balance;
+	int reqval;
+public:
+	InsuffException(int val, int req) : balance(val), reqval(req)
+	{}
+	void ShowExceptionInfo() const
+	{
+		cout << "잔액에서 " << reqval - balance << "가(이) 부족합니다!" << endl;
+	}
+};
+#endif
